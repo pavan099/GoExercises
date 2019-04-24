e04e8bb1c4c03c5d0dc603ee7c1215a994548a50
Chandan Ghosh <ckghosh1983@gmail.com>
2019-04-24 16:53:47 +0530

    little cleaner syntax

--- ex-template-composite-data/ex-slice-struct/main.go
+++ ex-template-composite-data/ex-slice-struct/main.go
@@ -22,11 +22,11 @@ func main() {
 	buddha := sage{"Buddha", "The belief of no beliefs"}
 	gandhi := sage{"Gandhi", "Be the change"}
 	mlk := sage{"Martin Luthar King", "Hatred never wins"}
+	jesus := sage{"Jesus", "Love is the only beautiful thing in the world!"}
 
-	sages := []sage{buddha, gandhi, mlk}
+	sages := []sage{buddha, gandhi, mlk, jesus}
 
-	err := tmpl.Execute(os.Stdout, sages)
-	if err != nil {
+	if err := tmpl.Execute(os.Stdout, sages); err != nil {
 		log.Fatalln(err)
 	}
 
