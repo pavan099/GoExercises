11554f583c9bc4a0fe4233bde6f92d8a08c787bc
Chandan Ghosh <ckghosh1983@gmail.com>
2019-04-29 09:28:30 +0530

    tcp server simple in-memory database

---
+++ tcp-apps/in-memory-db/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer func() {
+		err := listener.Close()
+		if err != nil {
+			log.Panicln(err)
+		}
+	}()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		go handle(connection)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Panicln(err)
+		}
+	}()
+
+	_, _ = io.WriteString(conn, "Welcome to the database\n")
+	_, _ = io.WriteString(conn,
+		"Use GET key \n"+
+			"SET key value \n"+
+			"DEL key\n")
+
+	scanner := bufio.NewScanner(conn)
+	data := make(map[string]string)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			words := strings.Fields(line)
+
+			if len(words) < 1 {
+				continue
+			}
+
+			switch words[0] {
+			case "GET":
+				key := words[1]
+				val := data[key]
+				_, _ = fmt.Fprintf(conn, "%s\r\n", val)
+
+			case "SET":
+				key := words[1]
+				val := words[2]
+				data[key] = val
+			case "DEL":
+				key := words[1]
+				delete(data, key)
+				_, _ = fmt.Fprintf(conn, "DELETED %s\n", key)
+			default:
+				_, _ = fmt.Fprintf(conn, "INVALID COMMAND %s\n", words)
+				continue
+			}
+
+		}
+	}
+	fmt.Println("Connection closed")
+}
